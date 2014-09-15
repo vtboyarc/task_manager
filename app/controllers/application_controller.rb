58cc8396631275cd2152bdcd2a4ba454a9f07e6c
Adam Carter <adamrcarter@gmail.com>
2014-09-15 15:18:30 -0500

    added authentication for user login sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :authorize
+  
+  def authorize
+    if current_user.nil?
+      redirect_to logins_path, :alert => "You need to log in first!"
+    end
+  end
+  
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+  #this allows the current_user method to be available in all the views
+  helper_method :current_user
+  
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ TaskManager::Application.routes.draw do
    root :to => 'pages#index'
    
    resources :users, :tasks
-  
+
 end
