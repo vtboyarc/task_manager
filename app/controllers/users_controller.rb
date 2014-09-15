abda7db061c4699ce90934f462fcdc8ad084fb5d
Tony Plaza <tplaza3@gmail.com>
2014-09-15 17:02:47 -0500

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,37 @@ class UsersController < ApplicationController
       raise "user wasn't saved"
     end
   end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      redirect_to users_path(@user.id)
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update_attributes(params[:user])
+      redirect_to users_path(@user.id)
+    else
+      render "edit"
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).delete
+    redirect_to users_path
+  end
 end
