2ae6c6920a3b86146ba57155ea8bf26b8a736358
Adam Carter <adamrcarter@gmail.com>
2014-09-15 16:27:00 -0500

    added changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest
+  attr_accessible :email, :name, :password, :password_confirmation
   
   has_secure_password
+  
+  validates_presence_of :password, :on => :create
+  
+  def current_user
+    if session[:user_id]
+      @user_id ||= User.find(session[:user_id])
+    end
+  end
 end
