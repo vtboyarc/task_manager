40bbeedfa8abffbb11bd77de30fbc9790bb5a619
“Alyssa <lasher1010@hotmail.com>
2014-09-15 14:26:05 -0500

    First commit YAY!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = 'dd43c3080de031c3ab5b6bc32623e58678200801526427ea7d08f1ea4286b106a774a4147fa8a2adc359ab54cc319efec54c1d7f17659c1f486c783eea73b1e7'
