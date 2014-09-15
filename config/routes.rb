e13396926e3e59fe23e44a5114ff1af139aa21d6
“Alyssa <lasher1010@hotmail.com>
2014-09-15 15:47:31 -0500

    Removed random junk

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ TaskManager::Application.routes.draw do
   
    root :to => 'pages#index'
    
-<<<<<<< HEAD
-   resources :users, :tasks
-=======
    get "login" => 'logins#new', :as => "login"
    post "login" => 'logins#create', :as => "create_login" 
    
@@ -14,6 +11,6 @@ TaskManager::Application.routes.draw do
    get "signup" => 'users#new', :as => "signup"
    
    resources :users, :tasks, :logins
->>>>>>> 851116494c5648c791388bb56c0f37d5a5ab29d3
+
 
 end
