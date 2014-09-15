191cb8955e828ba3744fd2b88b28882a033250fe
Adam Carter <adamrcarter@gmail.com>
2014-09-15 15:36:15 -0500

    fixed an authentication bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,7 @@
 class PagesController < ApplicationController
+   skip_before_filter :authorize, :only => [:new, :create]
+  def home
+  end
+  
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  skip_before_filter :authorize
+   skip_before_filter :authorize, :only => [:new, :create]
   
   def new
     @user = User.new
