6efb844be34e8e31d0f5a44f2ee56332930ed6c0
“Alyssa <lasher1010@hotmail.com>
2014-09-16 19:46:07 -0500

    Filled out comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,51 @@
 class CommentsController < ApplicationController
+  
+  skip_before_filter :authorize, :only => [:index, :show]
+ 
+  def index
+    @comments = Comment.all
+  end
+  
+  def new
+    @comment = Comment.new
+    @tasks = Tag.all
+    @users = User.all
+  end
+ 
+  def create
+    @comment = Comment.new(params[:comment])
+    
+    if @comment.save
+      redirect_to comments_path(@comment.id), :notice => "You have saved a new comment."
+    else
+      render "new"
+    end
+  end
+  
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+  
+  def update
+    @comment = Comment.find(params[:id])
+    
+    if @comment.update_attributes(params[:comment])
+      redirect_to comments_path(@comment.id), :notice => "You have upated this comment."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    redirect_to comments_path, :notice => "You have deleted this comment."
+  end
+
+  
+  def show
+    @comment = Comment.find(params[:id])
+    @user = User.find(@comment.user_id)
+    @task = Tag.find(@comment.task_id)
+  end
+  
 end
