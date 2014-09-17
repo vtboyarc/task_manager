92615a8673ca6f62e77ad2a239f5887027f57323
Tony Plaza <tplaza3@gmail.com>
2014-09-17 14:21:08 -0500

    fix delete/destroy!

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -36,6 +36,7 @@ class TagsController < ApplicationController
 
   def destroy
     @tag = Tag.find(params[:id])
+    @tag.destroy
     redirect_to tags_path, :notice => "You have deleted this tags."
   end
 
