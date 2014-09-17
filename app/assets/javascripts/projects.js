07e6b9c6075ae6c4015aebba0eb9035188d19431
Adam Carter <adamrcarter@gmail.com>
2014-09-17 17:52:01 -0500

    made a change that shows a users's name above their projects

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -4,26 +4,3 @@
     $( ".draggable" ).draggable({ snap: true, grid: [ 10,10 ] });
   
 });
-
-// to force it to drop in a certain place
-// $(function() {
-//     $("#draggable").draggable({
-//         revert : function(event, ui) {
-//
-//             $(this).data("uiDraggable").originalPosition = {
-//                 top : 0,
-//                 left : 0
-//             };
-//             // return boolean
-//             return !event;
-//
-//         }
-//     });
-//     $("#droppable").droppable();
-// });
-//
-// need this for droppable to work, in hmtml, under the last <%end%>
-// <div id="droppable">
-//     <p>Drop me here</p>
-// </div>
-//
