f0a40b48b2470ab818a5868015bc0ba531bf9dcf
“Alyssa <lasher1010@hotmail.com>
2014-09-19 13:58:15 -0500

    Changed some indentation

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,24 @@ TaskManager::Application.routes.draw do
   post "login" => 'logins#create', :as => "create_login" 
   get 'logout' => 'logins#destroy', :as => 'logout'
 
-   get "projects" => 'projects#index', :as => "projects"
-   get "projects/new" => 'projects#new', :as => "new_project"
-   
-   get "projects/:name/edit" => 'projects#edit', :as => "edit_project"
-   get "project/:name" => 'projects#show', :as => "project"
+  get "projects" => 'projects#index', :as => "projects"
+  get "projects/new" => 'projects#new', :as => "new_project"
   
-   post "projects" => 'projects#create' 
-   put "project/:name" => 'projects#update'
+  get "projects/:name/edit" => 'projects#edit', :as => "edit_project"
+  get "project/:name" => 'projects#show', :as => "project"
+  
+  post "projects" => 'projects#create' 
+  put "project/:name" => 'projects#update'
   
 
-   delete "projects/:name/delete" => 'projects#destroy', :as => "delete_project"
-   get "project/:name" => 'projects#show', :as => "project"
-   
-   
-   post "create_user" => 'users#create', :as => "create_user"
-     get "signup" => 'users#new', :as => "signup"
+  delete "projects/:name/delete" => 'projects#destroy', :as => "delete_project"
+  get "project/:name" => 'projects#show', :as => "project"
+  
+  
+  post "create_user" => 'users#create', :as => "create_user"
+  get "signup" => 'users#new', :as => "signup"
 
-   resources :users, :tasks, :tags, :comments
-   resources :activities
+  resources :users, :tasks, :tags, :comments
+  resources :activities
    
 end
