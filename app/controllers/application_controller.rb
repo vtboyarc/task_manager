15ff03e5c1db5791516c0263507be9bbd7c61aac
Tony Plaza <tplaza3@gmail.com>
2014-09-21 20:44:19 -0500

    fix delete on update and create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,11 @@ class ApplicationController < ActionController::Base
     # binding.pry
     if session[:user_id]
       @current_user ||= User.find_by_id(session[:user_id])
-      if @current_user.nil?
-        reset_session 
-        redirect_to login_path, :alert => "You need to log in first!"
-      end
+      # binding.pry
+      # if @current_user.nil?
+  #       reset_session
+  #       redirect_to login_path, :alert => "You need to log in first!"
+  #     end
     end
   end
   
